960eb964114d764d118e1bbd44af2e4ccfbfa063
viralsir <viralsir2018@gmail.com>
2020-12-16 16:46:32 +0530

    added 3 maxno file

---
+++ max_no_3.py
@@ -0,0 +1,10 @@
+no1=int(input("enter no1:"))
+no2=int(input("enter no2:"))
+no3=int(input("Enter no3:"))
+if no1>no2 and no1>no3:
+    print(no1," is a maximum no")
+elif no2>no3 :
+    print(no2," is a maximum no")
+else :
+    print(no3," is a maximum no")
+
