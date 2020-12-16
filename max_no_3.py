a941157cde1dd0eabb96a5477edfe48338cb4216
viralsir <viralsir2018@gmail.com>
2020-12-16 17:09:39 +0530

    added postive nagetive conditions

--- max_no_3.py
+++ max_no_3.py
@@ -1,10 +1,13 @@
 no1=int(input("enter no1:"))
 no2=int(input("enter no2:"))
 no3=int(input("Enter no3:"))
-if no1>no2 and no1>no3:
-    print(no1," is a maximum no")
-elif no2>no3 :
-    print(no2," is a maximum no")
-else :
-    print(no3," is a maximum no")
+if no1>0 and no2>0 and no3>0:
 
+    if no1>no2 and no1>no3:
+        print(no1," is a maximum no")
+    elif no2>no3 :
+        print(no2," is a maximum no")
+    else :
+        print(no3," is a maximum no")
+else:
+    print("nagetive no are not allowed.")
