fb2e116b06f89bbdf7d88a64772543bb051e343c
viralsir <viralsir2018@gmail.com>
2020-12-16 17:25:23 +0530

    adding label

--- max_no_3.py
+++ max_no_3.py
@@ -1,8 +1,9 @@
+print("\t\t maximum no program ")
 no1=int(input("enter no1:"))
 no2=int(input("enter no2:"))
 no3=int(input("Enter no3:"))
+print("output :\n")
 if no1>0 and no2>0 and no3>0:
-
     if no1>no2 and no1>no3:
         print(no1," is a maximum no")
     elif no2>no3 :
